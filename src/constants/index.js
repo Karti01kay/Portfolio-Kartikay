df991e2a793b1b7aefb6a48cbf3686e6ea0771f4
Kartikay Singh <98656350+Karti01kay@users.noreply.github.com>
2024-10-19 12:34:37 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,10 +15,6 @@ import {
     git,
     figma,
     docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
     carrent,
     jobit,
     tripguide,
@@ -42,11 +38,11 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Software Developer",
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Frontend Web Developer",
       icon: mobile,
     },
     {
@@ -116,11 +112,11 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
+      title: "Web Developer Intern",
+      company_name: "NullClass",
+      icon: web,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "July 2024 - September 2024",
       points: [
         "Developing and maintaining web applications using React.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
@@ -128,40 +124,15 @@ import {
         "Participating in code reviews and providing constructive feedback to other developers.",
       ],
     },
+    
     {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
+      title: "Full Stack Developer",
+      company_name: "Oasis Infobyte",
+      icon: web,
       iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
+      date: "March 2023 - August 2023",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Developing and maintaining applications using React.js, Next.js, Figma and the backend technologies including Express.js, Node.js, MongoDB and Flask.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
@@ -198,9 +169,9 @@ import {
   
   const projects = [
     {
-      name: "Car Rent",
+      name: "My Brainwave",
       description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        "Modern UI/UX website, exemplifies modern UI/UX principles. Its sleek design, seamless animations, and overall user experience set a high standard.",
       tags: [
         {
           name: "react",
@@ -216,12 +187,12 @@ import {
         },
       ],
       image: carrent,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/Karti01kay/My-brainWave",
     },
     {
-      name: "Job IT",
+      name: "Hospital Booking",
       description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        "A mern stack doctor appointment booking website using react.js, node.js, express.js, MongoDB, JWT, stripe payment gateway from scratch with modern UI and UX.",
       tags: [
         {
           name: "react",
@@ -237,12 +208,12 @@ import {
         },
       ],
       image: jobit,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/Karti01kay/Hospital-Booking",
     },
     {
       name: "Trip Guide",
       description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+        "A comprehensive travel booking platform that allows users to book trips and hotels, and offers curated recommendations for popular destinations.",
       tags: [
         {
           name: "nextjs",
@@ -258,7 +229,7 @@ import {
         },
       ],
       image: tripguide,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/Karti01kay/Tour-Management",
     },
   ];
   
