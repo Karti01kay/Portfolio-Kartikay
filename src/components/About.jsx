54a57975df3a5e418196b98cee67bb260f76ab27
Kartikay Singh <98656350+Karti01kay@users.noreply.github.com>
2024-11-10 15:44:11 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,9 +44,10 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
+        I'm a skilled developer with experience in Java, TypeScript, JavaScript, 
+        and expertise in frameworks like React, Next.js, Node.js, Express.js. 
+        Also familiar with databases including MongoDB, MySQL, PostgreSQL.
+        I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
